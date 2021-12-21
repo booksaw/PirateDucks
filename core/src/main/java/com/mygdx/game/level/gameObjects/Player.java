740b3d5d13b343640644a8be7f2cda1841539b7c
James McNair <booksaw224@gmail.com>
2021-12-21 09:52:39 +0000

    Added rotation to player

--- core/src/main/java/com/mygdx/game/level/gameObjects/Player.java
+++ core/src/main/java/com/mygdx/game/level/gameObjects/Player.java
@@ -9,17 +9,19 @@ import com.mygdx.game.level.GameObject;
 public class Player extends GameObject {
     //Body is an object yet to be defined which will be defined as the main Player
 
-    Texture texture;
+    private final Texture texture;
+    private float rotation;
 
     public Player() {
         super(100, 100);
         // loading the texture
         texture = new Texture(Gdx.files.internal("DuckBoat_TopView.png"));
+        rotation = 0;
     }
 
     @Override
     public void render(SpriteBatch batch) {
-        batch.draw(texture, x, y, width, height);
+        batch.draw(texture, x, y, width / 2, height/2, width, height, 1, 1, rotation, 0, 0, texture.getWidth(), texture.getHeight(), false, false);
     }
 
     private final double SPEED = 5;
@@ -31,17 +33,21 @@ public class Player extends GameObject {
 
         double deltaSpeed = SPEED + delta;
 
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.W) || Gdx.input.isKeyPressed(Input.Keys.UP)) {
             vel_y += deltaSpeed;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.S) || Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
             vel_y -= deltaSpeed;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             vel_x -= deltaSpeed;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             vel_x += deltaSpeed;
+        }
+
+        if(vel_x != 0 || vel_y != 0){
+           rotation = (float) Math.toDegrees(-Math.atan2(vel_x, vel_y));
         }
 
         x += vel_x;

a5eb12da59587acd687b78c96a7c1d67e97d7e5f
James McNair <booksaw224@gmail.com>
2021-12-29 12:57:18 +0000

    Made College a subclass of LevelManager

---
+++ core/src/main/java/io/github/pirateducks/level/MainLevel.java
@@ -0,0 +1,23 @@
+package io.github.pirateducks.level;
+
+import com.badlogic.gdx.graphics.Texture;
+import io.github.pirateducks.PirateDucks;
+
+public class MainLevel extends LevelManager {
+
+    public MainLevel(PirateDucks mainClass) {
+        super(mainClass);
+    }
+
+    @Override
+    protected Texture getMapTexture() {
+        return new Texture("map.png");
+    }
+
+    @Override
+    protected void setup() {
+        // nothing to do
+    }
+
+
+}

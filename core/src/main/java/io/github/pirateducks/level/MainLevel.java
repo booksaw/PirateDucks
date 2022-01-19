95e999a15d8926dcbdbae632ef2097218ac0d264
Marc Perales <49318468+Anon0x19@users.noreply.github.com>
2022-01-19 14:44:00 +0000

    Background music and Ocean sfx (#42)
    Added background music, ocean sfx and cannon ball fire sounds

--- core/src/main/java/io/github/pirateducks/level/MainLevel.java
+++ core/src/main/java/io/github/pirateducks/level/MainLevel.java
@@ -1,10 +1,15 @@
 package io.github.pirateducks.level;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import io.github.pirateducks.PirateDucks;
 
 public class MainLevel extends LevelManager {
+
+    public Music music;
+    public Music sfx_ocean;
 
     public MainLevel(PirateDucks mainClass) {
         super(mainClass);
@@ -17,8 +22,21 @@ public class MainLevel extends LevelManager {
 
     @Override
     protected void setup(OrthographicCamera camera) {
-        // nothing to do
+        //Sets the background music
+        music = Gdx.audio.newMusic(Gdx.files.internal("Main_Theme.ogg"));
+        music.setLooping(true);
+        music.setVolume(0.3f);
+        music.play();
+
+        sfx_ocean = Gdx.audio.newMusic(Gdx.files.internal("Ocean.ogg"));
+        sfx_ocean.setLooping(true);
+        sfx_ocean.setVolume(0.2f);
+        sfx_ocean.play();
     }
 
-
+    @Override
+    public void stopDisplaying() {
+        music.dispose();
+        sfx_ocean.dispose();
+    }
 }

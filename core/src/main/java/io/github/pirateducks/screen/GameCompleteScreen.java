d54811642e64dfcaee921df4a9c51fb2d2e74f83
Dan Wade <73236960+dan-wade42@users.noreply.github.com>
2022-01-31 11:34:41 +0000

    Add comments and refactor GameCompletedScreen code

--- core/src/main/java/io/github/pirateducks/screen/GameCompleteScreen.java
+++ core/src/main/java/io/github/pirateducks/screen/GameCompleteScreen.java
@@ -13,55 +13,55 @@ import io.github.pirateducks.PirateDucks;
 
 public class GameCompleteScreen implements Screen{
 
+    // Initialise Textures and Sprites for graphics
     private Texture backgroundTexture;
     private Sprite backgroundSprite;
-
     private Texture headerTexture;
     private Sprite headerSprite;
-
     private Texture menuButtonTexture;
     private Sprite menuButtonSprite;
+
 
     private OrthographicCamera camera;
     private PirateDucks mainClass;
 
+    /**
+     * Class constructor, called when this screen is to be displayed
+     * @param mainClass MainClass which holds the main game and player data
+     * @param camera Manages screen of the game
+     */
     public GameCompleteScreen(PirateDucks mainClass, OrthographicCamera camera) {
-
         this.camera = camera;
         this.mainClass = mainClass;
 
     }
 
 
-
     /**
-     * Called to draw the screen
+     * Called to draw the screen and display graphics
      *
-     * @param batch
+     * @param batch Used to draw sprites
      */
     @Override
     public void draw(SpriteBatch batch, OrthographicCamera camera) {
 
-        // set background as blurred map
+        // Set background as blurred map
         ScreenUtils.clear(0, 0, 0.2f, 1);
-
         backgroundTexture = new Texture("map_blurred.png");
         backgroundSprite = new Sprite(backgroundTexture);
         backgroundSprite.setSize(camera.viewportWidth, camera.viewportHeight);
         backgroundSprite.setPosition(0, 0);
 
-        if (backgroundSprite != null){
+        // Draw all graphics
+        if (backgroundSprite != null && headerSprite != null && menuButtonSprite != null){
             backgroundSprite.draw(batch);
-        }
-
-        if (headerSprite != null){
             headerSprite.draw(batch);
-        }
-
-        if (menuButtonSprite != null){
             menuButtonSprite.draw(batch);
+        } else {
+            throw new Error("Error displaying graphics, please restart game");
         }
 
+        // Generate and draw text which displays number of points user obtained
         BitmapFont font = new BitmapFont();
         font.getData().setScale(1.5f);
         String points = "You scored " + mainClass.points + " points!";
@@ -70,15 +70,15 @@ public class GameCompleteScreen implements Screen{
     }
 
     /**
-     * Called to update the screen
+     * Called to update the screen and check if buttons are pressed
      *
      * @param delta The delta time since the last update
      */
     @Override
     public void update(float delta) {
-
+        // User has left-clicked, check if they clicked on one of the buttons
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-            // Scale mouse position
+            // Get scaled mouse position
             Vector2 scaledMouse = PirateDucks.getScaledMouseLocation(camera);
 
             // Set button as menu button
@@ -92,6 +92,7 @@ public class GameCompleteScreen implements Screen{
 
             // Check if mouse is within the button positions
             if (scaledMouse.x >= buttonX && scaledMouse.x <= (buttonX + buttonW) && scaledMouse.y >= buttonY && scaledMouse.y <= (buttonY + buttonH)) {
+                // User has pressed return to main menu button
                 mainClass.setCurrentScreen(new MainMenuScreen(mainClass));
             }
         }
@@ -100,20 +101,24 @@ public class GameCompleteScreen implements Screen{
     }
 
     /**
-     * Called when this screen becomes the active screen
+     * Called when this screen becomes the active screen, this generates the graphics for the screen
      */
     @Override
     public void startDisplaying(OrthographicCamera camera) {
+        // Generate header texture and sprite
         headerTexture = new Texture("gameCompletedScreen/header.png");
         headerSprite = new Sprite(headerTexture);
 
+        // Scale and set position of header
         float scaleRatio = (headerSprite.getWidth() / camera.viewportWidth * 1.2f);
         headerSprite.setSize(headerSprite.getWidth() / scaleRatio, headerSprite.getHeight()/scaleRatio);
         headerSprite.setPosition(camera.viewportWidth/2 - headerSprite.getWidth()/2,(camera.viewportHeight/2 - headerSprite.getHeight()/2) * 1.9f);
 
+        // Generate menu button texture and sprite
         menuButtonTexture = new Texture("gameCompletedScreen/menu.png");
         menuButtonSprite = new Sprite(menuButtonTexture);
 
+        // Scale and set position of menu button
         scaleRatio = (menuButtonSprite.getWidth() / camera.viewportWidth * 2.0f);
         menuButtonSprite.setSize(menuButtonSprite.getWidth() / scaleRatio, menuButtonSprite.getHeight() / scaleRatio);
         menuButtonSprite.setPosition(camera.viewportWidth/2 - menuButtonSprite.getWidth()/2,(camera.viewportHeight/2 - menuButtonSprite.getHeight()/2) * 0.5f);
@@ -123,7 +128,7 @@ public class GameCompleteScreen implements Screen{
 
     /**
      * Called when this screen is no longer the active screen
-     * Use this method to dispose of everything
+     * This disposes of all graphics objects which are not needed
      */
     @Override
     public void stopDisplaying() {

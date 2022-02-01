baae78be5150b77db263b7302846bb1ac54b58ee
CharlieCrosley <charlie1crosley@gmail.com>
2022-02-01 18:10:25 +0000

    Increased window size

--- desktop/src/io/github/pirateducks/desktop/DesktopLauncher.java
+++ desktop/src/io/github/pirateducks/desktop/DesktopLauncher.java
@@ -8,12 +8,9 @@ import io.github.pirateducks.PirateDucks;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-//		config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
-//		config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
-		config.width = 848;
-		config.height = 480;
+		config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
+		config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
 		config.title = "Pirate Ducks";
-//		config.fullscreen = true;
 		config.addIcon("icons/DuckBoat_SideView_16x16.png", Files.FileType.Internal);
 		config.addIcon("icons/DuckBoat_SideView_32x32.png", Files.FileType.Internal);
 		config.addIcon("icons/DuckBoat_SideView_128x128.png", Files.FileType.Internal);
